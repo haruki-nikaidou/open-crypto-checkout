adcf9b4f9c9e12728653d24564c16493c7aa22ad
Nikaidou Haruki <nharuki@pm.me>
2026-02-03 20:33:53 +0900

    remove eq

--- ocrch-sdk/src/objects/mod.rs
+++ ocrch-sdk/src/objects/mod.rs
@@ -5,14 +5,14 @@ pub mod webhook;
 pub trait Signature: for<'de> serde::Deserialize<'de> + serde::Serialize {}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct SignedObject<T: Signature + Eq> {
+pub struct SignedObject<T: Signature> {
     pub body: T,
     pub timestamp: i64,
     pub json: String,
     pub signature: Box<[u8]>,
 }
 
-impl<T: Signature + Eq> SignedObject<T> {
+impl<T: Signature> SignedObject<T> {
     const MAX_AGE: i64 = 5 * 60;
     pub fn new(body: T, key: &[u8]) -> Result<Self, serde_json::Error> {
         let now = time::OffsetDateTime::now_utc().unix_timestamp();
@@ -31,10 +31,6 @@ impl<T: Signature + Eq> SignedObject<T> {
         })
     }
     pub fn verify(self, key: &[u8]) -> Result<T, SignedObjectParseError> {
-        let reparsed: T = serde_json::from_str(&self.json)?;
-        if reparsed != self.body {
-            return Err(SignedObjectParseError::BodyMismatch);
-        }
         let data = format!("{}.{}", self.timestamp, self.json);
         ring::hmac::verify(
             &ring::hmac::Key::new(ring::hmac::HMAC_SHA256, key),
@@ -53,8 +49,6 @@ impl<T: Signature + Eq> SignedObject<T> {
 pub enum SignedObjectParseError {
     #[error("invalid json: {0}")]
     Json(#[from] serde_json::Error),
-    #[error("body mismatch to json string")]
-    BodyMismatch,
     #[error("invalid signature")]
     SignatureMismatch(#[from] ring::error::Unspecified),
     #[error("timestamp too old")]
