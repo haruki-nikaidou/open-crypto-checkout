806240fc788600c581ee4ebd6ea8ec767f89ccb0
Nikaidou Haruki <nharuki@pm.me>
2026-02-11 03:17:53 +0900

    remove bad trait

--- ocrch-core/src/framework.rs
+++ ocrch-core/src/framework.rs
@@ -1,25 +1,5 @@
 use sqlx::PgPool;
 
-pub trait DatabaseAccessor {
-    fn acquire(&mut self) -> impl sqlx::PgExecutor<'_>;
-}
-
 pub struct DatabaseProcessor {
     pub pool: PgPool,
-}
-
-pub struct TransactionProcessor<'b> {
-    pub tx: sqlx::Transaction<'b, sqlx::Postgres>,
-}
-
-impl DatabaseAccessor for DatabaseProcessor {
-    fn acquire(&mut self) -> impl sqlx::PgExecutor<'_> {
-        &self.pool
-    }
-}
-
-impl<'b> DatabaseAccessor for TransactionProcessor<'b> {
-    fn acquire(&mut self) -> impl sqlx::PgExecutor<'_> {
-        &mut *self.tx
-    }
 }
